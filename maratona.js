71df3996813f6914363e96ceb687dbf71c97e229
Mariii <139721644+mariiiporfirio@users.noreply.github.com>
2024-10-13 20:54:45 -0300

    Pastas html e js.

---
+++ maratona.js
@@ -0,0 +1,15 @@
+/* CONDICIONAIS
+1. Escreva um programa que peça ao usuário um número e informe se ele 
+está entre 1 e 100. */
+let usuario_numero = prompt('Digite um número:');
+
+2. Implemente uma função que recebe a idade de uma pessoa e retorna uma 
+mensagem informando se ela é criança, adolescente ou adulta.
+
+
+3. Crie um programa que solicite um mês (de 1 a 12) e informe quantos dias 
+esse mês possui.
+
+
+4. Escreva um programa que peça ao usuário um número e verifique se ele é 
+positivo, negativo ou zero.
